1f2d2dbe5ebcb5f7f57528a91c7ca3ef8ce3b7ab
Lockerio <goryunov.arkadiy@gmail.com>
2024-09-05 21:48:46 +0300

    Update README.md

--- src/app/parsers/detmir_parser.py
+++ src/app/parsers/detmir_parser.py
@@ -16,51 +16,47 @@ class DetmirParser:
         options = undetected_chromedriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument("--start-maximized")
-        options.binary_location = "/usr/bin/google-chrome"
+        # options.binary_location = "/usr/bin/google-chrome"
         self.driver = undetected_chromedriver.Chrome(options=options)
 
-        self.blocks_1_css_selector = ".tO.tV.bNf.t_"
-        self.blocks_2_css_selector = ".tO.tV.bM_1.t_"
-        self.blocks_3_css_selector = ".tO.tV.bM_5.t_"
-        self.blocks_4_css_selector = ".Y_._5x"
-        self.imgs_4_css_selector = ".QG.QJ.QK"
-        self.blocks_5_css_selector = ".er.hk"
+        self.blocks_1_css_selector = ".sY.s_4.bPq.s_9"
+        self.blocks_2_css_selector = ".sY.s_4.bO_4.s_9"
+        self.blocks_3_css_selector = ".sY.s_4.bO_8.s_9"
+        self.blocks_4_css_selector = ".iC.iL"
+        self.imgs_4_css_selector = ".CM.CP.CQ"
+        self.blocks_5_css_selector = ".sa.fm"
 
     def get_detmir_data(self) -> list[dict[str, Any]]:
         self.driver.get(self.url)
         time.sleep(3)
         data = []
 
-        web_block_1 = WebDriverWait(self.driver, 15).until(
+        web_block_1 = WebDriverWait(self.driver, 25).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.blocks_1_css_selector))
         )
         block_1 = self.parse_block(web_block_1, 1)
         data.extend(block_1)
 
-        web_block_2 = WebDriverWait(self.driver, 15).until(
+        web_block_2 = WebDriverWait(self.driver, 25).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.blocks_2_css_selector))
         )
         block_2 = self.parse_block(web_block_2, 2)
         data.extend(block_2)
 
-        web_block_3 = WebDriverWait(self.driver, 15).until(
+        web_block_3 = WebDriverWait(self.driver, 25).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.blocks_3_css_selector))
         )
         block_3 = self.parse_block(web_block_3, 3)
         data.extend(block_3)
 
-        web_block_4 = WebDriverWait(self.driver, 15).until(
+        web_block_4 = WebDriverWait(self.driver, 25).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.blocks_4_css_selector))
         )[-1]
         element_4 = web_block_4.find_elements(By.CSS_SELECTOR, self.imgs_4_css_selector)
         block_4 = self.parse_block(element_4, 4)
         data.extend(block_4)
 
-        web_block_5 = WebDriverWait(self.driver, 15).until(
+        web_block_5 = WebDriverWait(self.driver, 25).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, self.blocks_5_css_selector))
         )
         block_5 = self.parse_adv_block(web_block_5, 5)
